5214154e459b2272a39ffd8e367c5e384aca1c54
William Eiffert <weiffert@gmail.com>
2016-02-03 10:18:00 -0600

    Basic Game Loop template
    Sources: gafferongames and gameprogrammingpatterns

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,6 @@
 //includes
 #include "Game.h"
+#include <time.h>
 
 Game::Game()
 {
@@ -24,15 +25,37 @@ int Game::run()
 
 int Game::gameLoop()
 {
+	bool running = true;
+
 	//initialize time keepers
+	double totaltime = 0.0;
+	double frameRate = 0.01;
+
+	double lag = 0.0;
+	double currentTime = time(NULL);
+
+	while(running)
 	{
 		//initialize current time keepers
+		double newTime = time(NULL);
+		double frameTime = newTime - currentTime;
+		
+		if (frameTime > 0.25)
+			frameTime = 0.25;
+
+		currentTime = newTime;
+
+		lag += frameTime;
 		//process input
+		while (lag >= frameRate)
 		{
 			//update
 			//decrement current time keepers
+			totalTime += frameRate;
+			lag -= frameRate;
 		}
 		//render with parameters.
+		//render(lag/frameRate);
 	}
 	return exitCode;
 }
