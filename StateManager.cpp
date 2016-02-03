603c118ffeee5696a23064a80a1c02eb6bc9c211
teron146 <adamsawyer1@hotmail.com>
2016-02-03 10:09:21 -0600

    Revert "Revert "State Manager Prototype""
    This reverts commit ed351724ffe805f44b1fb2b06868fbf6fc1efd91.

---
+++ StateManager.cpp
@@ -0,0 +1,27 @@
+#include "StateManager.h"
+
+
+StateManager::StateManager()
+{
+}
+
+
+StateManager::~StateManager()
+{
+}
+
+void StateManager::update()
+{
+}
+
+void StateManager::render()
+{
+}
+
+const int StateManager::getNumber()
+{
+}
+
+const std::string StateManager::getId()
+{
+}
---
+++ StateManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Entity.h"
+
+class StateManager
+{
+public:
+	StateManager();
+	~StateManager();
+	virtual void update() = 0;
+	virtual void render() = 0;
+	virtual const int getNumber() = 0;
+	virtual const std::string getId() = 0;
+private:
+	std::vector<*Entity> material;
+	std::vector<*sf::Image> assetImage;
+	std::vector<*sf::Sound> assetSound;
+	const int number;
+	const std::string id;
+
+};
+
