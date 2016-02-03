8dc951aadc676d63c0c4207f22cdfdf461dc265c
teron146 <adamsawyer1@hotmail.com>
2016-02-03 10:02:35 -0600

    StateManager Prototype
    Basic prototype for the StateManager class. Mostly just abstract
    classes.

---
+++ StateManager.cpp
@@ -0,0 +1,28 @@
+#include "StateManager.h"
+
+
+StateManager::StateManager()
+{
+}
+
+
+StateManager::~StateManager()
+{
+}
+
+//All abstract classes below.
+void StateManager::update()
+{
+}
+
+void StateManager::render()
+{
+}
+
+const int StateManager::getNumber()
+{
+}
+
+const std::string StateManager::getId()
+{
+}
