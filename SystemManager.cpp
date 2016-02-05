289b33ab5f4636a51dfee609d518246af89751aa
zanedurante <zanedurante@gmail.com>
2016-02-05 14:57:55 -0600

    SystemManager.cpp without corrections
    This most likely will not compile yet I will fix it

---
+++ SystemManager.cpp
@@ -0,0 +1,54 @@
+#include "SystemManager.h"
+#include <vector>
+
+
+SystemManager::SystemManager()
+{
+}
+
+
+SystemManager::~SystemManager()
+{
+  //Delete vectors
+}
+
+
+//Add a vector of entities
+void SystemManager::addMaterial()
+{
+
+}
+
+
+//Add a BaseComponent
+void SystemManager::addComponent()
+{
+
+}
+
+
+//Add a BaseController
+void SystemManager::addController()
+{
+
+}
+
+
+std::vector<Entity*> SystemManager::getMaterial()
+{
+
+}
+
+
+std::vector<BaseComponent*> SystemManager::getComponent()
+{
+
+}
+
+
+std::vector<BaseController*> SystemManager::getController()
+{
+
+}
+
+
