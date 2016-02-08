62a263313b81e8aa974c6effde220017b1714459
zanedurante <zanedurante@gmail.com>
2016-02-08 16:03:21 -0600

    Updated Entity.h
    Changed "vector" to <vector>
    Added std:: in front of string
    Changed * to the end of the pointers

--- Entity.h
+++ Entity.h
@@ -1,26 +1,30 @@
 #pragma once
 #include <string>
-#include "vector"
-#include "BaseComponent"
-#include "BaseController"
+#include <vector>
+#include "BaseComponent.h"
+#include "BaseController.h"
+
+
 class Entity
 {
 private:
-	const string id;
+	std::string id;
 	std::vector<BaseComponent*> components;
 	std::vector<BaseController*> controllers;
 
 public:
-	string getId();
-	string setId(string);
+	Entity();
+	~Entity();
+	std::string getId();
+	void setId(std::string);
 
-	void addComponent(*BaseComponent);
+	void addComponent(BaseComponent*);
 	void removeComponent();
-	*BaseComponent getComponent();
+	BaseComponent* getComponent();
 	bool hasComponent();
 
-	void addController(*BaseController);
+	void addController(BaseController*);
 	void removeController();
-	*BaseController getController();
+	BaseController* getController();
 	bool hasCOntroller();
 };
