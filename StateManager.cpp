50ca8f633042eab664aaa90ca382a31d7d849a4d
zanedurante <zanedurante@gmail.com>
2016-02-08 16:28:16 -0600

    Updated Functions NOT COMPLETE

--- StateManager.cpp
+++ StateManager.cpp
@@ -1,6 +1,7 @@
 #include "StateManager.h"
 #include <string>
 
+
 StateManager::StateManager()
 {
 }
@@ -10,18 +11,36 @@ StateManager::~StateManager()
 {
 }
 
+
 void StateManager::update()
 {
 }
+
 
 void StateManager::render()
 {
 }
 
+
 int StateManager::getNumber()
 {
+	return number;
 }
+
 
 std::string StateManager::getId()
 {
+	return id;
+}
+
+
+void StateManager::changeState(int)
+{
+
+}
+
+
+void StateManager::changeState(std::string)
+{
+
 }
