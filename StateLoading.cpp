2e951fe3381571268443e4897e84beb385d3d634
zanedurante <zanedurante@gmail.com>
2016-02-11 09:38:19 -0600

    Update StateLoading.cpp
    Changed to fit current documentation

--- StateLoading.cpp
+++ StateLoading.cpp
@@ -1,12 +1,12 @@
-#include "stdafx.h"
 #include "StateLoading.h"
-#include "BaseState.h"
+
 
 StateLoading::StateLoading()
 {
-	number = 1;
 	id = "StateLoading";
+	number = 1;
 }
+
 
 StateLoading::~StateLoading()
 {
