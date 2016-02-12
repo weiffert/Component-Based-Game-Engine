f703a9b5a855c884f611e094451977bebabc97fa
William Eiffert <weiffert@gmail.com>
2016-02-12 07:54:49 -0600

    Update StateWelcome.cpp

--- StateWelcome.cpp
+++ StateWelcome.cpp
@@ -11,3 +11,15 @@ StateWelcome::StateWelcome()
 StateWelcome::~StateWelcome()
 {
 }
+
+void StateWelcome::update()
+{
+	//Wait time step amount.
+}
+
+
+void StateWelcome::render(double lag)
+{
+	//entities and move controller. (move controller will take the time and move the entity a certain amount.)
+	//Render entities.
+}
