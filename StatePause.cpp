4d179467743265021272f0e4ab745dbead03927f
William Eiffert <weiffert@gmail.com>
2016-02-12 08:25:13 -0600

    Update StatePause.cpp

--- StatePause.cpp
+++ StatePause.cpp
@@ -11,3 +11,22 @@ StatePause::StatePause()
 StatePause::~StatePause()
 {
 }
+
+
+int StatePause::update()
+{
+	//same event handling as StateMenu.
+	//If return to game,
+		//return 2;
+	//If exit,
+		//return -1;
+	//else
+		//return 1;
+}
+
+
+void StatePause::render(double lag)
+{
+	//Render the entities using the render controller.
+	//Look something like render.control(*Entity, lag);
+}
