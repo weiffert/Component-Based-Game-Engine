5b8d9b32269e6ee7b53202f616e096bd2bd8b0a0
William Eiffert <weiffert@gmail.com>
2016-02-12 07:59:41 -0600

    Update StateLoading.cpp

--- StateLoading.cpp
+++ StateLoading.cpp
@@ -11,3 +11,18 @@ StateLoading::StateLoading()
 StateLoading::~StateLoading()
 {
 }
+
+
+void StateLoading::update()
+{
+	//check which states have been initialized in SystemManager's vector of BaseState.
+		//There are keywords for figuring out the state's class. Try typeof or typeid
+	//Initialize the state that is next in numeric order.
+}
+
+
+void StateLoading::render(double lag)
+{
+	//Render the entities using the render controller.
+	//Look something like render.control(*Entity, lag);
+}
