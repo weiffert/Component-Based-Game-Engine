62b0dca8c248a6cc9a978f80faf5f6a7e986d57b
William Eiffert <weiffert@gmail.com>
2016-02-12 08:09:11 -0600

    Update StateLoading.cpp

--- StateLoading.cpp
+++ StateLoading.cpp
@@ -17,7 +17,11 @@ int StateLoading::update()
 {
 	//check which states have been initialized in SystemManager's vector of BaseState.
 		//There are keywords for figuring out the state's class. Try typeof or typeid
-	//Initialize the state that is next in numeric order.
+	//If not all of the states have been initialized...
+		//Initialize the state that is next in numeric order.
+		//return 1;
+	//Else...all of the states are made.
+		//return -1;
 }
 
 
