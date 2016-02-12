661c992e18d3eef361848d6f86c2b8030ebeb152
William Eiffert <weiffert@gmail.com>
2016-02-12 08:06:58 -0600

    Changed update to return an int.
    This is so that we have a testable value. Also, added pseudo code.

--- StateMenu.cpp
+++ StateMenu.cpp
@@ -11,3 +11,23 @@ StateMenu::StateMenu()
 StateMenu::~StateMenu()
 {
 }
+
+
+int StateMenu::update()
+{
+	//Check for arrow key and space bar events.
+		//Arrow keys for navigation, space bar for selecting.
+	//If arrow key event, change properties of the entities.
+		//One looses the selector property and one gains it. Only one will have the selector property at a time.
+	//If space bar event is the level, __________________________________________
+		//return 1;
+	//If the space bar event is quit, quit.
+		//return -1;		-1 is kind of the standard for quitting it seems.
+}
+
+
+void StateMenu::render(double lag)
+{
+	//Render the entities using the render controller.
+	//Look something like render.control(*Entity, lag);
+}
