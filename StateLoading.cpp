9459fc135e8620d8ad3a148f94a012e1a2621554
William Eiffert <weiffert@gmail.com>
2016-02-14 17:27:32 -0600

    Update StateLoading.cpp

--- StateLoading.cpp
+++ StateLoading.cpp
@@ -1,10 +1,11 @@
 #include "StateLoading.h"
 
 
-StateLoading::StateLoading()
+StateLoading::StateLoading(SystemManager * a)
 {
 	id = "Loading";
 	number = 0;
+	systemManager = a;
 }
 
 
@@ -15,13 +16,17 @@ StateLoading::~StateLoading()
 
 int StateLoading::update()
 {
-	//check which states have been initialized in SystemManager's vector of BaseState.
-		//There are keywords for figuring out the state's class. Try typeof or typeid
-	//If not all of the states have been initialized...
-		//Initialize the state that is next in numeric order.
-		//return 1;
-	//Else...all of the states are made.
-		//return -1;
+	//check if all of the entities have been initialized.
+	//If not all of the entitites have been intitalized...
+		//Do one.
+	//If all of them have been, move on.
+		//check which states have been initialized in SystemManager's vector of BaseState.
+			//There are keywords for figuring out the state's class. Try typeof or typeid
+		//If not all of the states have been initialized...
+			//Initialize the state that is next in numeric order.
+			//return 1;
+		//Else...all of the states are made.
+			//return -1;
 }
 
 
