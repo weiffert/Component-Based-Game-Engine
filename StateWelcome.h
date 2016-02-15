bd437a8e09118f995f22686f2add98bcefa155ac
William Eiffert <weiffert@gmail.com>
2016-02-15 13:57:23 -0600

    Added SystemManager linking

--- StateWelcome.h
+++ StateWelcome.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "BaseState.h"
+#include "SystemManager.h"
 class StateWelcome :
 	public BaseState
 {
 public:
-	StateWelcome();
+	StateWelcome(SystemManager *);
 	~StateWelcome();
 };
 
