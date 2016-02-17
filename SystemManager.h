bf9edd62b2b0dd6cba7c5debe24fde58b9ee89cc
zanedurante <zanedurante@gmail.com>
2016-02-17 09:14:45 -0600

    Update SystemManager.h

--- SystemManager.h
+++ SystemManager.h
@@ -9,18 +9,19 @@ public:
 	SystemManager();
 	~SystemManager();
 	void addMaterial(Entity*, StateManager*);
-	void addComponent();
-	void addController();
+	void addComponent(BaseComponent *);
+	void addController(BaseController *);
 	void addState(BaseState*);
-	void deleteMaterial(string);
-	void deleteComponent(string);
-	void deleteController(string);
-	void deleteState(string);
-	Entity* getMaterial(string);
+	void deleteMaterial(std::string);
+	void deleteComponent(std::string);
+	void deleteController(std::string);
+	void deleteState(std::string);
+	void deleteState(int);
+	Entity* getMaterial(std::string);
 	std::vector<Entity*> getMaterial(StateManager*);
-	BaseComponent* getComponent(string);
-	BaseController* getController(string);
-	BaseState* getState(string);
+	BaseComponent* getComponent(std::string);
+	BaseController* getController(std::string);
+	BaseState* getState(std::string);
 	
 private:
 	std::vector<vector<Entity*>> material;
