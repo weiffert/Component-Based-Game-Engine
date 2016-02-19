c7b8c9b870c50d5c9ce4a0b8644a05229f9f8dab
weiffert <weiffert@gmail.com>
2016-02-18 20:22:04 -0600

    Rough Implementation

--- main.cpp
+++ main.cpp
@@ -1,19 +1,38 @@
 /*
 Authors: William Eiffert, Adam Sawyer, Zane Durante, Kendrick Austin
-Date: 3 February 2016
+Date Begin: 3 February 2016
+Last Edit: 18 February 2016
 Purpose: This is a game engine designed to use entities and component based design.
 The implementation of the engine will be missile command.
 */
 
 //includes
-
+#include "Game.h"
+#include <fstream>
+#include <time.h>
 
 int main()
 {
+	int exitCode = 0;
+	
 	//Log run
-	//create game
-	//run game
+	ostream file;
+	file.open("log.txt"; ios::app);
+	if (file.is_open())
+		cout << "Program run at: " << time(NULL);
+
+	//Create the game.
+	Game game;
+
+	//run the game
+	exitCode = game.run();
+
 	//log exit code
+	if (file.is_open())
+		cout << "\nExit Code: " << exitCode << endl;
+
 	//clean up
+	file.close();
+	
 	return 0;
 }
