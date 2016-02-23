0b5fb764e7352971e859f20d2ed9937b46382c19
zanedurante <zanedurante@gmail.com>
2016-02-23 09:37:25 -0600

    Update StateLoading.cpp
    Changed switch statements to if else statements

--- StateLoading.cpp
+++ StateLoading.cpp
@@ -161,52 +161,40 @@ void StateLoading::update(double totalTime, sf::RenderWindow *window)
 
 			//Create.
 			BaseComponent *temp;
-			switch (type)
-			{
-			case "int":
+		
+			if (type == "int")
 				temp = new Property<int>();
-				break;
 
-			case "char":
+			else if (type == "char")
 				temp = new Property<char>();
-				break;
 
-			case "double":
+			else if (type == "double")
 				temp = new Property<double>();
-				break;
 
-			case "float":
+			else if (type == "float")
 				temp = new Property<float>();
-				break;
 
-			case "bool":
+			else if (type == "bool")
 				temp = new Property<bool>();
-				break;
 
-			case "string":
+			else if (type == "string")
 				temp = new Property<std::string>();
-				break;
 
-			case "Entity":
+			else if (type == "Entity")
 				temp = new Property<Entity>();	//Does this work?
-				break;
 
-			case "Texture":
+			else if (type == "Texture")
 				temp = new Property<sf::Texture>();
-				break;
 
-			case "Image":
+			else if (type == "Image")
 				temp = new Property<sf::Image>();
-				break;
 
-			case "Sound":
+			else if (type == "Sound")
 				temp = new Property<sf::Sound>();
-				break;
 
-			default:
+			else
 				temp = new Property<std::string>();
 
-			}
 
 			//Edit.
 			temp->setId(id);
@@ -269,55 +257,54 @@ void StateLoading::update(double totalTime, sf::RenderWindow *window)
 						else
 						{
 							//Needs to be converted to the proper type.
-							switch (type)
-							{
-							case "int":
-								temp->getComponent(properties.at(y).at(0))->changeData(static_cast<int>(properties.at(y).at(x)) - 48, x - 1);
-								break;
+							if (type == "int")
+								temp->getComponent(properties.at(y).at(0))->changeData
+								(static_cast<int>(properties.at(y).at(x)) - 48, x - 1);
 
-							case "char":
-								temp->getComponent(properties.at(y).at(0))->changeData(properties.at(y).at(x).at(1), x - 1);
-								break;
+							else if (type == "char")
+								temp->getComponent(properties.at(y).at(0))->changeData
+								(properties.at(y).at(x).at(1), x - 1);
 
-							case "double":
-								temp->getComponent(properties.at(y).at(0))->changeData(static_cast<double>(properties.at(y).at(x)) - 48, x - 1);
-								break;
+							else if (type == "double")
+								temp->getComponent(properties.at(y).at(0))->changeData
+								(static_cast<double>(properties.at(y).at(x)) - 48, x - 1);
 
-							case "float":
-								temp->getComponent(properties.at(y).at(0))->changeData(static_cast<float>(properties.at(y).at(x)) - 48, x - 1);
-								break;
-
-							case "bool":
-								bool tOrF = false;
-								if (properties.at(y).at(x) == "true")
-									tOrF = true;
-								temp->getComponent(properties.at(y).at(0))->changeData(tOrF, x - 1);
-								break;
-
-							case "string":
-								temp->getComponent(properties.at(y).at(0))->changeData(properties.at(y).at(x), x - 1);
-								break;
-
-							case "Entity":
-								temp->getComponent(properties.at(y).at(0))->changeData(systemManager->getMaterial(properties.at(y).at(x)), x - 1);
-								break;
-
-							case "Texture":
-								temp->getComponent(properties.at(y).at(0))->changeData(assetManager->getTexture(properties.at(y).at(x)), x - 1);
-								break;
-
-							case "Image":
-								temp->getComponent(properties.at(y).at(0))->changeData(assetManager->getComponent(properties.at(y).at(x)), x - 1);
-								break;
-
-							case "Sound":
-								temp->getComponent(properties.at(y).at(0))->changeData(assetManager->getSound(properties.at(y).at(x)), x - 1);
-								break;
-
-							default:
-								temp->getComponent(properties.at(y).at(0))->changeData(properties.at(y).at(x), x - 1);
-
-							}
+							else if (type == "float")
+								temp->getComponent(properties.at(y).at(0))->changeData
+								(static_cast<float>(properties.at(y).at(x)) - 48, x - 1);
+			
+							else if (type == "bool")
+								{
+									bool tOrF = false;
+									if (properties.at(y).at(x) == "true")
+										tOrF = true;
+									temp->getComponent(properties.at(y).at(0))->changeData
+									(tOrF, x - 1);
+								}
+		
+							else if (type == "string")
+								temp->getComponent(properties.at(y).at(0))->changeData
+								(properties.at(y).at(x), x - 1);
+				
+							else if (type == "Entity")
+								temp->getComponent(properties.at(y).at(0))->changeData
+								(systemManager->getMaterial(properties.at(y).at(x)), x - 1);
+				
+							else if (type == "Texture")
+								temp->getComponent(properties.at(y).at(0))->changeData
+								(assetManager->getTexture(properties.at(y).at(x)), x - 1);
+				
+							else if (type == "Image")
+								temp->getComponent(properties.at(y).at(0))->changeData
+								(assetManager->getComponent(properties.at(y).at(x)), x - 1);
+				
+							else if (type == "Sound")
+								temp->getComponent(properties.at(y).at(0))->changeData
+								(assetManager->getSound(properties.at(y).at(x)), x - 1);
+				
+							else
+								temp->getComponent(properties.at(y).at(0))->changeData
+								(properties.at(y).at(x), x - 1);
 						}
 					}
 					//Link the property.
