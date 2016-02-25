44f9fd1b83299fbde3f92301fcceec29c0ba97fb
weiffert <weiffert@gmail.com>
2016-02-24 18:11:36 -0600

    Semi-finished typing

--- StateLoading.cpp
+++ StateLoading.cpp
@@ -185,43 +185,87 @@ void StateLoading::update(double totalTime, sf::RenderWindow *window)
 			}
 				
 			else if (type == "char")
+			{
 				temp = new Property<char>();
 
+			}
+
 			else if (type == "double")
+			{
 				temp = new Property<double>();
 
+			}
+
 			else if (type == "float")
+			{
 				temp = new Property<float>();
 
+			}
+
+			//Rest of the statements are done.
 			else if (type == "bool")
+			{
 				temp = new Property<bool>();
 
+				bool tOrF = false;
+				if (data.at(0) == "true")
+					tOrF = true;
+				temp->getComponent(properties.at(y).at(0))->addData(tOrF);
+			}
+
 			else if (type == "string")
+			{
 				temp = new Property<std::string>();
+
+				for (int i = 0; i < data.size(); i++)
+					temp->addData(data.at(i));
+
+			}
 
 			else if (type == "Entity")
-				temp = new Property<Entity>();	//Does this work?
-								//temp->getComponent(id) store this as data
+			{
+				temp = new Property<Entity>();
+
+				for (int i = 0; i < data.size(); i++)
+					temp->add(systemManager->getEntity(data.at(i));
+			}
 
 			else if (type == "Texture")
+			{
 				temp = new Property<sf::Texture>();
+				
+				for (int i = 0; i < data.size(); i++)
+					temp->add(assetManager->getTexture(std::stoi(data.at(i)));
+			}
 
 			else if (type == "Image")
+			{
 				temp = new Property<sf::Image>();
 
+				for (int i = 0; i < data.size(); i++)
+					temp->add(assetManager->getTexture(data.at(i));
+			}
+
 			else if (type == "Sound")
+			{
 				temp = new Property<sf::Sound>();
 
+				for (int i = 0; i < data.size(); i++)
+					temp->add(assetManager->getSound(data.at(i));
+			}
+
 			else
+			{
 				temp = new Property<std::string>();
+
+				for (int i = 0; i < data.size(); i++)
+					temp->addData(data.at(i));
+
+			}
 
 
 			//Edit.
 			temp->setId(id);
-			//Adding data.
-			for (int i = 0; i < data.size(); i++)
-				temp->addData(data.at(i));
-
 			//Store.
 			systemManager->add(temp);
 
@@ -278,53 +322,83 @@ void StateLoading::update(double totalTime, sf::RenderWindow *window)
 						{
 							//Needs to be converted to the proper type.
 							if (type == "int")
+							{
 								temp->getComponent(properties.at(y).at(0))->changeData
-								(static_cast<int>(properties.at(y).at(x)) - 48, x - 1);
+									(static_cast<int>(properties.at(y).at(x)) - 48, x - 1);
+
+							}
 
 							else if (type == "char")
+							{
 								temp->getComponent(properties.at(y).at(0))->changeData
-								(properties.at(y).at(x).at(1), x - 1);
+									(properties.at(y).at(x).at(1), x - 1);
+							}
 
 							else if (type == "double")
+							{
 								temp->getComponent(properties.at(y).at(0))->changeData
-								(static_cast<double>(properties.at(y).at(x)) - 48, x - 1);
+									(static_cast<double>(properties.at(y).at(x)) - 48, x - 1);
+
+							}
 
 							else if (type == "float")
+							{
 								temp->getComponent(properties.at(y).at(0))->changeData
-								(static_cast<float>(properties.at(y).at(x)) - 48, x - 1);
-			
+									(static_cast<float>(properties.at(y).at(x)) - 48, x - 1);
+
+							}
+							
+							//Rest of the statements are done.
 							else if (type == "bool")
-								{
-									bool tOrF = false;
-									if (properties.at(y).at(x) == "true")
-										tOrF = true;
-									temp->getComponent(properties.at(y).at(0))->changeData
+							{
+								bool tOrF = false;
+								if (properties.at(y).at(x) == "true")
+									tOrF = true;
+								temp->getComponent(properties.at(y).at(0))->changeData
 									(tOrF, x - 1);
-								}
+							}
 		
 							else if (type == "string")
+							{
 								temp->getComponent(properties.at(y).at(0))->changeData
-								(properties.at(y).at(x), x - 1);
+									(properties.at(y).at(x), x - 1);
+
+							}
 				
 							else if (type == "Entity")
+							{
 								temp->getComponent(properties.at(y).at(0))->changeData
-								(systemManager->getMaterial(properties.at(y).at(x)), x - 1);
+									(systemManager->getMaterial(properties.at(y).at(x)), x - 1);
+
+							}
 				
 							else if (type == "Texture")
+							{
 								temp->getComponent(properties.at(y).at(0))->changeData
-								(assetManager->getTexture(properties.at(y).at(x)), x - 1);
+									(assetManager->getTexture(std::stoi(properties.at(y).at(x))), x - 1);
+
+							}
 				
 							else if (type == "Image")
+							{
 								temp->getComponent(properties.at(y).at(0))->changeData
-								(assetManager->getComponent(properties.at(y).at(x)), x - 1);
+									(assetManager->getComponent(properties.at(y).at(x)), x - 1);
+
+							}
 				
 							else if (type == "Sound")
+							{
 								temp->getComponent(properties.at(y).at(0))->changeData
-								(assetManager->getSound(properties.at(y).at(x)), x - 1);
+									(assetManager->getSound(properties.at(y).at(x)), x - 1);
+
+							}
 				
 							else
+							{
 								temp->getComponent(properties.at(y).at(0))->changeData
-								(properties.at(y).at(x), x - 1);
+									(properties.at(y).at(x), x - 1);
+
+							}
 						}
 					}
 					//Link the property.
