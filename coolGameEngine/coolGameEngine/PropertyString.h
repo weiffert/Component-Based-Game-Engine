ca10acd2c4b04c1b1be4c7ce3ca53b5fd7270e70
William Eiffert <weiffert@gmail.com>
2016-03-01 20:15:32 -0600

    Update PropertyString.h

--- coolGameEngine/coolGameEngine/PropertyString.h
+++ coolGameEngine/coolGameEngine/PropertyString.h
@@ -1,10 +1,27 @@
 #pragma once
 #include "BaseComponent.h"
+
+#include <string>
+
 class PropertyString :
 	public BaseComponent
 {
 public:
 	PropertyString();
 	~PropertyString();
+
+	//Returns the data as a vector.
+	std::vector<std::string> getData();
+	std::string getData(int);
+
+	//Edit the data.
+	void addData(std::string);
+	void deleteData();
+	void deleteData(std::string);
+	void deleteDataPosition(int);
+	void changeData(std::string, int);
+
+private:
+	std::vector<float> data;
 };
 
