0dd90fbcb0f5b90acc765a9393ea9c79984e80ed
zanedurante <zanedurante@gmail.com>
2016-03-11 10:09:30 -0600

    Update MissileLauncher.h

--- coolGameEngine/coolGameEngine/MissileLauncher.h
+++ coolGameEngine/coolGameEngine/MissileLauncher.h
@@ -4,4 +4,11 @@ class MissileLauncher
 public:
 	MissileLauncher();
 	~MissileLauncher();
+	int getMissiles();
+	void fire();
+	void setMissilesLeft();
+	void setTotalMissiles();
+private:
+	int missilesLeft;
+	int totalMissiles;
 };
