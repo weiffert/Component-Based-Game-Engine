8d5d28dcf3040156006e65afcea488706231e0d0
zanedurante <zanedurante@gmail.com>
2016-03-11 10:17:38 -0600

    Update MissileLauncher.cpp

--- coolGameEngine/coolGameEngine/MissileLauncher.cpp
+++ coolGameEngine/coolGameEngine/MissileLauncher.cpp
@@ -11,8 +11,38 @@ MissileLauncher::~MissileLauncher()
 }
 
 
+//Fires a missile
+void MissileLauncher::fire()
+{
+  //Shoots missile if it has missiles left
+  
+  //If it doesn't, do not fire and possibly tell the user
+  
+  //Decrease current missile count
+  
+}
+
+
+//Sets the amount of missiles left in the missile launcher to an int 
+void MissileLauncher::setMissilesLeft(int a)
+{
+  missilesLeft = a;
+}
+
+//Sets the amount of missiles left in the missile launcher to an int 
+void MissileLauncher::setTotalMissiles(int a)
+{
+  totalMissiles = a;
+}
+
+
 int MissileLauncher::getMissilesLeft()
 {
   return missilesLeft;
 }
 
+
+int MissileLauncher::getTotalMissiles()
+{
+  return totalMissiles;
+}
