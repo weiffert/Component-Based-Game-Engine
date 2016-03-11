469b752aa838cc1225b7d33bcc1d146e1160e9df
kta25298 <kendrickaustin@me.com>
2016-03-11 09:50:44 -0600

    Create MissileLauncherAi.h

---
+++ coolGameEngine/coolGameEngine/MissileLauncherAi.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+
+#inlcude "SFML/Graphics.hpp"
+
+class MissileLauncherAi
+{
+public:
+
+std::vector<EnemyMissiles> activeMissiles;
+int currentMissile, totalMissile;
+
+private:
+
+MissileLauncherAi();
+
+MissileLauncherAi(int, int);
+
+void changeTotal(int);
+void changeCurrent(int);
+void launchMissiles();
+void checkForMissiles();
+
+~MissleLauncherAi();
+
