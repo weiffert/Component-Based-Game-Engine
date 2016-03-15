8079b46713b0b391a6bafc63842b5868c6d00111
kta25298 <kendrickaustin@me.com>
2016-03-15 10:18:49 -0500

    Create MissileGuidance.cpp
    in progress

---
+++ coolGameEngine/coolGameEngine/MissileGuidance.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+#include "MissileGuidance.h"
+
+
+MissileGuidance::MissileGuidance()
+{
+
+}
+
+MissileGuidance::MissileGuidance(int xCord)
+{
+  pathX = xCord;
+}
+
+void MissileGuidance::findBase()
+{
+
+}
