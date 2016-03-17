890a8d51f71c150127e6be576cc8d3968eb22d03
zanedurante <zanedurante@gmail.com>
2016-03-17 10:18:44 -0500

    Update MissileLauncher.h

--- coolGameEngine/coolGameEngine/MissileLauncher.h
+++ coolGameEngine/coolGameEngine/MissileLauncher.h
@@ -4,16 +4,18 @@ class MissileLauncher
 public:
 	MissileLauncher();
 	~MissileLauncher();
-	void fire();
+	int fire();	//Returns 0 if it fails to launch, 1 if it successfully launches
 	void setMissilesLeft(int);
 	void setTotalMissiles(int);
 	int getMissilesLeft();
 	int getTotalMissiles();
 	void setSloap();
 	sf::Vector2f getSloap();
+	void update();
 private:
 	int missilesLeft;
 	int totalMissiles;
 	int pathX, pathY;
 	sf::Vector2f sloap;
+	std::vector<Missile*> activeMissiles;
 };
