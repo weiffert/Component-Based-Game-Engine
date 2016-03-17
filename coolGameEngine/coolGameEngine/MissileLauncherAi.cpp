2c736c5226d91fbc6e813677449932cf3ceff04f
kta25298 <kendrickaustin@me.com>
2016-03-17 09:35:04 -0500

    Update MissileLauncherAi.cpp

--- coolGameEngine/coolGameEngine/MissileLauncherAi.cpp
+++ coolGameEngine/coolGameEngine/MissileLauncherAi.cpp
@@ -54,6 +54,58 @@ void MissileLauncherAi::launchMissiles()
   }
 }
 
+void MissileGuidance::selectBase()
+{
+  srand(time(NULL));
+  
+  int x = rand() % 3 + 1, y;
+  
+  if(x == 3)
+  {
+    x = rand() % (base image width) + 1;
+    
+    pathX = (screen width) - (distance between base and right edge) - x;
+    
+    y = rand() % (base image height) + 1;
+    
+    pathY = (screen height) - (distance between base and screen bottom) - y;
+  }
+  else if(x == 2)
+  {
+     x = rand() % (base image width) + 1;
+    
+    pathX = (screen width) - (distance between base and right edge) - x;
+    
+    y = rand() % (base image height) + 1;
+    
+    pathY = (screen height) - (distance between base and screen bottom) - y;
+  }
+  else
+  {
+    x = rand() % (base image width) + 1;
+    
+    pathX = (screen width) - (distance between base and right edge) - x;
+    
+    y = rand() % (base image height) + 1;
+    
+    pathY = (screen height) - (distance between base and screen bottom) - y;
+  }
+}
+
+void MissileGuidance::setSloap()
+{
+  double speedX;
+  
+  speedX = double(pathX) / double(pathY);
+  
+  sloap = (speedX, 1);
+}
+
+sf::Vector2f MissileGuidance::getSloap()
+{
+  return sloap;
+}
+
 bool MissileLauncherAi::checkForMissiles()
 {
    for(int x = 0; x < activeMissiles.size(); x++)
