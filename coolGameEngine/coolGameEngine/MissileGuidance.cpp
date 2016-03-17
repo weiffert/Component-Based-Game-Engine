0af14c6c1a2b72dfdc0b1a18b0b6bd73d391eeb4
kta25298 <kendrickaustin@me.com>
2016-03-17 09:38:36 -0500

    Update MissileGuidance.cpp

--- coolGameEngine/coolGameEngine/MissileGuidance.cpp
+++ coolGameEngine/coolGameEngine/MissileGuidance.cpp
@@ -1,5 +1,8 @@
 #include "stdafx.h"
 
+#include <stdlib.h>
+#include <time.h>
+
 #include "MissileGuidance.h"
 
 
@@ -13,7 +16,59 @@ MissileGuidance::MissileGuidance(int xCord)
   pathX = xCord;
 }
 
-void MissileGuidance::findBase()
+void MissileGuidance::selectBase()
 {
+  srand(time(NULL));
+  
+  int x = rand() % 3 + 1, y;
+  
+  if(x == 3)
+  {
+    x = rand() % (base image width) + 1;
+    
+    pathX = (screen width) - (distance between base and right edge) - x;
+    
+    y = rand() % (base image height) + 1;
+    
+    pathY = (screen height) - (distance between base and screen bottom) - y;
+  }
+  else if(x == 2)
+  {
+     x = rand() % (base image width) + 1;
+    
+    pathX = (screen width) - (distance between base and right edge) - x;
+    
+    y = rand() % (base image height) + 1;
+    
+    pathY = (screen height) - (distance between base and screen bottom) - y;
+  }
+  else
+  {
+    x = rand() % (base image width) + 1;
+    
+    pathX = (screen width) - (distance between base and right edge) - x;
+    
+    y = rand() % (base image height) + 1;
+    
+    pathY = (screen height) - (distance between base and screen bottom) - y;
+  }
+}
 
+void MissileGuidance::setSloap()
+{
+  double speedX;
+  
+  speedX = double(pathX) / double(pathY);
+  
+  sloap = (speedX, 1);
+}
+
+sf::Vector2f MissileGuidance::getSloap()
+{
+  return sloap;
+}
+
+MissileGuidance::~MissileGuidance()
+{
+  
 }
