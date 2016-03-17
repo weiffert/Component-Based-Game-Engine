658bd2523881f912ca7a3835f3828966cbd5771c
teron146 <adamsawyer1@hotmail.com>
2016-03-17 09:45:01 -0500

    Track Ball functionality
    I believe that this should properly read a track ball. If any problems
    present themselves while using a track ball I believe it will most
    likely be inverted movement. If this happens it can easily be fixed by
    switching the signs used for comparison in the if statements.

--- coolGameEngine/coolGameEngine/StateLevel.cpp
+++ coolGameEngine/coolGameEngine/StateLevel.cpp
@@ -49,38 +49,33 @@ void StateLevel::update(double totalTime, sf::RenderWindow* window)
 	sf::Event event;
 	while (window->pollEvent(event))
 	{
+		centerCoordinates.x = (window->getSize().x) / 2;
+		centerCoordinates.y = (window->getSize().y) / 2;
+		mouse.setPosition(centerCoordinates, *window);
 		bool moveUp = false, moveDown = false, moveRight = false, moveLeft = false, spaceBarReleased = false;
 
 		//Checks if up arrow key pressed
-		if ((event.type = sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Up))
-		{
+		if (mouse.getPosition.y > centerCoordinates.y)
 			moveUp = true;
-		}
 		//Checks if down arrow key pressed
-		if ((event.type = sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Down))
-		{
+		if (mouse.getPosition.y < centerCoordinates.y)
 			moveDown = true;
-		}
 		//Checks if right arrow key pressed
-		if ((event.type = sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Right))
-		{
+		if (mouse.getPosition.x > centerCoordinates.x)
 			moveRight = true;
-		}
 		//Checks if left arrow key pressed
-		if ((event.type = sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Left))
-		{
+		if (mouse.getPosition.x < centerCoordinates.x)
 			moveLeft = true;
-		}
 		//Checks if space bar released
-		if ((event.type = sf::Event::KeyReleased) && (event.key.code == sf::Keyboard::Space))
-		{
+		if ((event.type == sf::Event::KeyReleased) && (event.key.code == sf::Keyboard::Space))
 			spaceBarReleased = true;
-		}
 		//Checks if escape key pressed
-		if ((event.type = sf::Event::KeyReleased) && (event.key.code == sf::Keyboard::Escape))
-		{
+		if ((event.type == sf::Event::KeyReleased) && (event.key.code == sf::Keyboard::Escape))
 			BaseState::changeState(this, "Pause");
-		}
+		if (event.type == sf::Event::Closed)
+			window->close();
+
+		
 		//Run through the game controllers.
 		//Example: Checking for collisions
 		//systemManager->getController("PlayerInput")->control(moveUp, moveDown, moveRight, moveLeft, spaceBarReleased, &material);
