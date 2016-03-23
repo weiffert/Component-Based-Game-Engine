e4edd0f7d574f5441fa4d4da58a06de22f4df127
zanedurante <zanedurante@gmail.com>
2016-03-23 10:23:02 -0500

    Create MissileExploder.cpp

---
+++ coolGameEngine/coolGameEngine/MissileExploder.cpp
@@ -0,0 +1,16 @@
+#include "MissileExploder.h"
+
+MissileExploder::MissileExploder()
+{
+}
+
+
+MissileExploder::~MissileExploder()
+{
+}
+
+
+void MissileExploder::control(sf::Window &window, Entity * missile)
+{
+  //Make it go boom
+}
