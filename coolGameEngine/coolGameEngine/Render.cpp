78413711fe8dc615984463e51c2b9515312dfc5c
William Eiffert <weiffert@gmail.com>
2016-04-05 09:21:16 -0500

    No longer permanently moves the visuals.

--- coolGameEngine/coolGameEngine/Render.cpp
+++ coolGameEngine/coolGameEngine/Render.cpp
@@ -24,59 +24,58 @@ Render::~Render()
 void Render::control(double lag, sf::RenderWindow *window, Entity *a)
 {
 	//Temp stores the property to be drawn.
-	Property *temp, *temp2, *temp3;
+	Property *temp;
 	temp = nullptr;
-	temp2 = nullptr;
-	temp3 = nullptr;
+	int *x, *y;
+	*x = 0;
+	*y = 0;
 	//Check if it can be drawn.
 	if (a->hasComponent("draw"))
 	{
 		if (a->hasComponent("Sprite"))
 		{
-				temp = a->getComponent("Sprite");
-				bool move = movement(lag, a, temp2, temp3);
-				if (move)
-					temp->getDataSprite().at(0)->move(temp3->getDataDouble().at(0), temp3->getDataDouble().at(1));
-				window->draw(*(temp->getDataSprite().at(0)));
+			temp = a->getComponent("Sprite");
+			if (movement(lag, a, x, y)
+				temp->getDataSprite().at(0)->move(*x, *y);
+			window->draw(*(temp->getDataSprite().at(0)));
 		}
 		if (a->hasComponent("Text"))
 		{
 			temp = a->getComponent("Text");
-			bool move = movement(lag, a, temp2, temp3);
-			if (move)
-				temp->getDataText().at(0)->move(temp3->getDataDouble().at(0), temp3->getDataDouble().at(1));
+			if (movement(lag, a, x, y)
+				temp->getDataText().at(0)->move(*x, *y);
 			window->draw(*(temp->getDataText().at(0)));
 		}
 		if (a->hasComponent("CircleShape"))
 		{
 			temp = a->getComponent("CircleShape");
-			bool move = movement(lag, a, temp2, temp3);
-			if (move)
-				temp->getDataCircleShape().at(0)->move(temp3->getDataDouble().at(0), temp3->getDataDouble().at(1));
+			if (movement(lag, a, x, y)
+				temp->getDataCircleShape().at(0)->move(*x, *y);
 			window->draw(*(temp->getDataCircleShape().at(0)));
 		}
 		if (a->hasComponent("ConvexShape"))
 		{
 			temp = a->getComponent("ConvexShape");
-			bool move = movement(lag, a, temp2, temp3);
-			if (move)
-				temp->getDataConvexShape().at(0)->move(temp3->getDataDouble().at(0), temp3->getDataDouble().at(1));
+			if (movement(lag, a, x, y)
+				temp->getDataConvexShape().at(0)->move(*x, *y);
 			window->draw(*(temp->getDataConvexShape().at(0)));
 		}
 		if (a->hasComponent("RectangleShape"))
 		{
 			temp = a->getComponent("RectangleShape");
-			bool move = movement(lag, a, temp2, temp3);
-			if (move)
-				temp->getDataRectangleShape().at(0)->move(temp3->getDataDouble().at(0), temp3->getDataDouble().at(1));
+			if (movement(lag, a, x, y)
+				temp->getDataRectangleShape().at(0)->move(*x, *y);
 			window->draw(*(temp->getDataRectangleShape().at(0)));
 		}
 	}
 }
 
 
-bool Render::movement(double lag, Entity* a, Property* temp2, Property* temp3)
+bool Render::movement(double lag, Entity* a, int* i, int* j)
 {
+	Property *temp2, temp3;
+	temp2 = nullptr;
+	temp3 = nullptr;
 	//Update position based on time lag.
 	double x, y, changeX, changeY;
 	if (a->hasComponent("velocity"))
@@ -98,8 +97,8 @@ bool Render::movement(double lag, Entity* a, Property* temp2, Property* temp3)
 			x += changeX;
 			y += changeY;
 			//Store new position.
-			temp3->changeData(&x, 0);
-			temp3->changeData(&y, 1);
+			*i = x;
+			*j = y;
 
 			return true;
 		}
