6e4a0a4ee6924b2e65b1d5013b3666fd75981fbe
kta25298 <kendrickaustin@me.com>
2016-04-05 10:10:17 -0500

    Update MissileLauncherAi.h

--- coolGameEngine/coolGameEngine/MissileLauncherAi.h
+++ coolGameEngine/coolGameEngine/MissileLauncherAi.h
@@ -2,19 +2,20 @@
 class MissileLauncherAi
 {
 private:
-  int missilesLeft, totalMissiles, targetOne, targetTwo, targetThree;
+	int missilesLeft, totalMissiles, targetOne, targetTwo, targetThree;
 
 public:
-  MissileLauncherAi();
-  MissileLauncherAi(int, int);
-  ~MissleLauncherAi();
-  void setTargets();
-  double setSlope(Entity*, int, int);
-  void setTotalMissiles(int);
-  void setMissilesLeft(int);
-  int getMissilesLeft();
+	MissileLauncherAi();
+	MissileLauncherAi(int, int);
+	~MissleLauncherAi();
+	void setTargets();
+	int launchAmmount();
+	double setSlope(Entity*, int, int);
+	void setTotalMissiles(int);
+	void setMissilesLeft(int);
+	int getMissilesLeft();
 	int getTotalMissiles();
-  int launchMissiles(Entity *, Entity *, sf::Window & ); //Returns 0 if it fails to launch, 1 if it successfully launches
-  void update();
+	int launchMissiles(Entity *, Entity *, sf::Window & ); //Returns 0 if it fails to launch, 1 if it successfully launches
+	void update();
   
 
