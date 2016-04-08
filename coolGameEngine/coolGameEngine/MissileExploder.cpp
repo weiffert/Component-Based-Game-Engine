2bf1eb0c0faa873f660436c23e611b811f754e1a
zanedurante <zanedurante@gmail.com>
2016-04-07 20:22:29 -0500

    Update MissileExploder.cpp
    It might be easier if we do not switch through all the colors when creating the explosions.  If we still want to flash through all the colors, this update function will have to be run through much quicker and the variable rate should be changed accordingly.

--- coolGameEngine/coolGameEngine/MissileExploder.cpp
+++ coolGameEngine/coolGameEngine/MissileExploder.cpp
@@ -15,19 +15,16 @@ void MissileExploder::control(sf::Window &window, Entity * missile)
 {
   double rate;
   rate = 1;
+  double tempRadius;
   if (!(missile->getComponent("ExplosionPhase").at(0) == -2)) //Makes sure the explosion didn't already happen
   {
       //If it doesn't already have an explosion, create one (property "ExplosionPhase" is 0)
      if (missile->getComponent("ExplosionPhase").at(0) == 0)
      {
       missile->getComponent("ExplosionPhase")->deleteData();
-      missile->getComponent("ExplosionRadius")->addDataInt(1);
-      tempRadius = missile->getComponent("ExplosionRadius")->getDataDouble().at(0);
-      tempRadius += rate * missile->getComponent("ExplosionPhase")->getDataDouble().at(0);
-      missile->getComponent("ExplosionRadius")->deleteData();
-      missile->getComponent("ExplosionRadius")->addDataInt(tempRadius);
+      missile->getComponent("ExplosionPhase")->addDataInt(1);
       }
-  
+      
       //Check radius
       if (missile->getComponent("ExplosionRadius")->getDataInt().at(0) == 20)
       {
@@ -37,16 +34,19 @@ void MissileExploder::control(sf::Window &window, Entity * missile)
     //Check to see if the explosion disappears
     if(missile->getComponent("ExplosioRadius").at(0) - 
     rate * missile->getComponent("ExplosionPhase").at(0) < 0)
-  {
-       missile->getComponent("ExplosionRadius")->deleteData();
+    {
+    missile->getComponent("ExplosionRadius")->deleteData();
        missile->getComponent("ExplosionRadius")->addDataDouble(0);
        missile->getComponent("ExplosionPhase")->deleteData();
        missile->getComponent("ExplosionPhase")->addDataInt(-2);
       
     }
+    else
+    {
+      tempRadius = missile->getComponent("ExplosionRadius")->getDataDouble().at(0);
+      tempRadius += rate * missile->getComponent("ExplosionPhase")->getDataDouble().at(0);
+      missile->getComponent("ExplosionRadius")->deleteData();
+      missile->getComponent("ExplosionRadius")->addDataInt(tempRadius);
+    }
   }
-  
-  //change the size 
-  
-  
 }
