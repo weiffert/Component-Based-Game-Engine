ef8afbb6d3c7f19f0d0b3d1874ed24790a348057
zanedurante <zanedurante@gmail.com>
2016-04-07 21:17:42 -0500

    Create MissileChecker.cpp

---
+++ coolGameEngine/coolGameEngine/MissileChecker.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include "SystemManager.h"
+MissileChecker::MissileChecker()
+{
+}
+
+MissileChecker::~MissileChecker()
+{
+}
+
+void MissileChecker::control(sf::Window * window, SystemManger * systemManager)
+{
+  //Goes through each enemy missile and determines if they should explode, if they should explode call on MissileExploder
+  
+}
