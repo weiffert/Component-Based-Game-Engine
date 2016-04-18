6dc127a791b395ee93fa6d4fbc8ed9bc269fb6ae
kta25298 <kendrickaustin@me.com>
2016-04-18 18:31:33 -0500

    Create ScoreKeeper.cpp

---
+++ coolGameEngine/coolGameEngine/ScoreKeeper.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+
+
+
+ScoreKeeper::ScoreKeeper()
+{
+
+}
+
+ScoreKeeper::~ScoreKeeper()
+
+
+void buildCity();
+
+void increaseScore(int);
+
+void checkForCity();
+
+int totalScore;
+
+int stockedCities;
