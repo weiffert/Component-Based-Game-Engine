bcffe1e44c91146de3759bab00be091d24a60a6f
kta25298 <kendrickaustin@me.com>
2016-04-19 11:35:44 -0500

    Update ScoreKeeper.h

--- coolGameEngine/coolGameEngine/ScoreKeeper.h
+++ coolGameEngine/coolGameEngine/ScoreKeeper.h
@@ -1,10 +1,24 @@
 #pragma once
 
-#include ""
+#include "SystemManager.h"
+#include "Entity.h"
 
 class ScoreKeeper
 {
-
+  private:
+  
+  int recentScore;
+  int stockedCities;
+  
+  public:
+  
+  ScoreKeeper();
+  ~ScoreKeeper();
+  
+  void increaseScore(int, Entity*);
+  void printScore();
+  void buildCities();
+  
 
 
 };
