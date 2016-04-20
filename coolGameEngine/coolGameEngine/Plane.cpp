5ed2d55142ae8a1c4cc456d17c9923a8a3551a20
zanedurante <zanedurante@gmail.com>
2016-04-19 23:16:01 -0500

    Planes not perfect
    sf::Texture problems but the planes behave how they should.  They appear
    as white boxes.  They will also explode when hit by a missile.  The game
    crashes very early however to some pointer error.  I had trouble with
    github committing so I decided to copy and paste the code into here,
    that is probably why there is an error.

--- coolGameEngine/coolGameEngine/Plane.cpp
+++ coolGameEngine/coolGameEngine/Plane.cpp
@@ -16,11 +16,11 @@
 
 Plane::Plane()
 {
-  totalMissiles = 10;
-  missilesLeft = 10;
+	totalMissiles = 10;
+	missilesLeft = 10;
 
-  systemManager = nullptr;
-  assetManager = nullptr;
+	systemManager = nullptr;
+	assetManager = nullptr;
 }
 
 
@@ -31,15 +31,15 @@ Plane::Plane(SystemManager *s, AssetManager *a)
 }
 
 
-Plane::Plane(int totalMis, int currMis, int height , SystemManager * s, AssetManager *a)
+Plane::Plane(int totalMis, int currMis, int height, SystemManager * s, AssetManager *a)
 {
-  totalMissiles = totalMis;
-  missilesLeft = currMis;
-  systemManager = s;
-  assetManager = a;
-  systemManager->getMaterial("Plane")->getComponent("CurrentPosition")->deleteData();
-  systemManager->getMaterial("Plane")->getComponent("CurrentPosition")->addData(height);
-  systemManager->getMaterial("Plane")->getComponent("CurrentPosition")->addData(0);
+	totalMissiles = totalMis;
+	missilesLeft = currMis;
+	systemManager = s;
+	assetManager = a;
+	systemManager->getMaterial("Plane")->getComponent("CurrentPosition")->deleteData();
+	systemManager->getMaterial("Plane")->getComponent("CurrentPosition")->addData(height);
+	systemManager->getMaterial("Plane")->getComponent("CurrentPosition")->addData(0);
 }
 
 
@@ -50,10 +50,10 @@ Plane::~Plane()
 
 
 int Plane::launchMissiles(sf::RenderWindow *window)
-{		
+{
 	Entity * plane = nullptr;
 	Entity *missile = nullptr;
-	std::vector<Entity *> missiles = systemManager->getMaterial("LauncherAi")->getComponent("MissilesHeld")->getDataEntity();
+	std::vector<Entity *> missiles = systemManager->getMaterial("MissileLauncherAi")->getComponent("MissilesHeld")->getDataEntity();
 	MissileLauncherAi launcher;
 
 	//Go through each active plane
@@ -74,7 +74,7 @@ int Plane::launchMissiles(sf::RenderWindow *window)
 				//Checks if there are missiles left to fire
 				int decrement = 29;
 				bool found = false;
-				
+
 				while (!found && decrement >= 0)
 				{
 					if (missiles.at(decrement)->hasComponent("Fired"))
@@ -96,6 +96,7 @@ int Plane::launchMissiles(sf::RenderWindow *window)
 					missile->getComponent("SplitFired")->deleteData();
 					missile->getComponent("SplitFired")->addData(true);
 
+					//Fire from plane
 					missile->getComponent("StartingPosition")->deleteData();
 					missile->getComponent("StartingPosition")->addData(plane->getComponent("CurrentPosition")->getDataDouble().at(0));
 					missile->getComponent("StartingPosition")->addData(plane->getComponent("CurrentPosition")->getDataDouble().at(1));
@@ -106,8 +107,6 @@ int Plane::launchMissiles(sf::RenderWindow *window)
 
 					launcher.launchMissiles(missile, window);
 				}
-
-					//Set as split fired so it does not start at the top
 			}
 		}
 	}
@@ -118,11 +117,11 @@ int Plane::launchMissiles(sf::RenderWindow *window)
 
 double Plane::setSlope(double pathX, double pathY)
 {
-  double speedX;
-  
-  speedX = pathX / pathY;
-  
-  return speedX;
+	double speedX;
+
+	speedX = pathX / pathY;
+
+	return speedX;
 }
 
 
@@ -133,11 +132,12 @@ void Plane::update(sf::RenderWindow *window)
 	double length;
 	int planeNumber = 1;
 	sf::Vector2f rectLength;
-	sf::RectangleShape *temp = nullptr;
+	Entity *temp = nullptr;
 	sf::Sprite * plane = nullptr;
 	Entity * currentPlane = nullptr;
 	MissileLauncherAi missileLauncherAi(assetManager, systemManager);
 	std::vector<Entity*> missiles;
+	bool collision = false;
 
 	do
 	{
@@ -159,6 +159,30 @@ void Plane::update(sf::RenderWindow *window)
 
 		if (currentPlane->getComponent("Draw")->getDataBool().at(0)) //Makes sure current plane is active
 		{
+			//Checks for collisions
+			std::vector<Entity *> explodingMissiles = systemManager->getComponent("ExplodingMissiles")->getDataEntity();
+			for (int u = 0; u < explodingMissiles.size(); u++)
+			{
+				temp = explodingMissiles.at(u);
+				//Makes sure that explosion is not done and has not started
+				if (temp->getComponent("Explode")->getDataBool().at(0))
+				{
+					if (temp->hasComponent("CircleShape"))
+					{
+						if (this->intersection((temp->getComponent("CircleShape")->getDataCircleShape().at(0)), currentPlane->getComponent("Sprite")->getDataSprite().at(0)))
+						{
+							collision = true;
+						}
+					}
+				}
+			}
+			if (collision)
+			{
+				currentPlane->getComponent("Draw")->deleteData();
+				currentPlane->getComponent("Draw")->addData(false);
+				currentPlane->getComponent("Life")->deleteData();
+				currentPlane->getComponent("Life")->addData(false);
+			}
 
 			//Moves current plane
 			if (currentPlane->getComponent("Direction")->getDataString().at(0) == "Right")
@@ -173,11 +197,17 @@ void Plane::update(sf::RenderWindow *window)
 			currentPlane->getComponent("CurrentPosition")->changeData(&temp1, 0);
 
 			//If it's off the screen kill the plane
-			if (temp1 < 0)
+			if (temp1 < 0 || temp1 > 480)
 			{
 				currentPlane->getComponent("Life")->deleteData();
 				currentPlane->getComponent("Life")->addData(false);
+				currentPlane->getComponent("Draw")->deleteData();
+				currentPlane->getComponent("Draw")->addData(false);
 			}
+
+			//Call on launcher
+			this->launchMissiles(window);
+
 
 		}
 		planeNumber++;
@@ -208,6 +238,7 @@ void Plane::launchPlane(sf::RenderWindow * window, int planeNumber)
 	std::string * direction = new std::string; //Avoids linker errors
 	srand(time(NULL));
 
+
 	if (planeNumber == 1)
 	{
 		plane = systemManager->getMaterial("Plane1");
@@ -223,12 +254,13 @@ void Plane::launchPlane(sf::RenderWindow * window, int planeNumber)
 		plane = systemManager->getMaterial("Plane3");
 	}
 
+	sf::Sprite * satellite = plane->getComponent("Sprite")->getDataSprite().at(0);
 	//Sets draw to true which shows it is on the screen
 	plane->getComponent("Draw")->deleteData();
 	plane->getComponent("Draw")->addData(true);
 
 	//Determines which way the plane will face
-	
+
 	plane->getComponent("CurrentPosition")->deleteData();
 
 	//Left
@@ -248,28 +280,103 @@ void Plane::launchPlane(sf::RenderWindow * window, int planeNumber)
 		plane->getComponent("Direction")->changeData(direction, 0);
 
 		//Give correct x value
-		plane->getComponent("CurrentPosition")->addData(-1); 
+		plane->getComponent("CurrentPosition")->addData(-1);
 	}
 
 	//Give "random" y height below 400 and above 150
-	while (yHeight > 150)
+	do {
 		yHeight = rand() % 400;
+	} while (yHeight > 150);
+
 	plane->getComponent("CurrentPosition")->addData(yHeight);
 
-	//Make it either a satellite or a plane (default is plane
+	//Make it either a satellite or a plane (default is plane)  >>>>>>>>>>>> THESE TEXTURES ARE NOT WORKING <<<<<<<<<<<<<
 	if (rand() % 2 == 0)
 	{
 		//Make it a Satellite
+		sf::Texture texture;
+		if (texture.loadFromFile("Satellite.png"))
+			satellite->setTexture(texture);
+		else
+		{
+			if (texture.loadFromFile("Plane.png"))
+				satellite->setTexture(texture);
+		}
 		sf::Sprite *satellite = plane->getComponent("Sprite")->getDataSprite().at(0);
-		sf::Texture *texture = new sf::Texture;
-		if (!texture->loadFromFile("satellite.png"))
+		if (!texture.loadFromFile("satellite.png"))
 			std::cout << "Failed to load satellite.png" << std::endl;
-		satellite->setTexture(*texture);
+		satellite->setTexture(texture);
 
-		assetManager->add(texture);
+		assetManager->add(&texture);
 	}
 
-	
+	//This is done just so you can see a white box where the plane should be
+	sf::Texture texture;
+	texture.loadFromFile("Satellite.png");
+	satellite->setTexture(texture);
+
+	//Scale the plane
+	float scaleY = 30 / satellite->getLocalBounds().height;
+	float scaleX = 40 / satellite->getLocalBounds().width;
+	satellite->setScale(sf::Vector2f(scaleX, scaleY));
+
+	//Set other values
 
 	delete direction;
+}
+
+
+bool Plane::intersection(sf::CircleShape * explosion, sf::Sprite * plane)
+{
+	//Checks if sprite contains any "extreme" point on the circle (top, bottom, far left, far right)
+	bool collision = false;
+	sf::Vector2f center = { sf::Vector2f((sf::FloatRect(explosion->getGlobalBounds()).left) + (sf::FloatRect(explosion->getGlobalBounds()).width / 2),
+		(sf::FloatRect(explosion->getGlobalBounds()).top) + sf::FloatRect(explosion->getGlobalBounds()).height / 2) };
+	sf::Vector2f distance;
+
+	//Checks if the circle is inside
+	if (plane->getGlobalBounds().contains(center))
+		collision = true;
+
+	//Far right
+	if (plane->getGlobalBounds().contains(sf::Vector2f(center.x + explosion->getRadius(), center.y)))
+		collision = true;
+	//Far Left
+	if (plane->getGlobalBounds().contains(sf::Vector2f(center.x - explosion->getRadius(), center.y)))
+		collision = true;
+	//Bottom
+	if (plane->getGlobalBounds().contains(sf::Vector2f(center.x, center.y + explosion->getRadius())))
+		collision = true;
+	//Top
+	if (plane->getGlobalBounds().contains(sf::Vector2f(center.x, center.y - explosion->getRadius())))
+		collision = true;
+
+	//Check if circle contains the extreme points of the sprite (each corner)
+	sf::Vector2f topLeftCorner = { plane->getGlobalBounds().top, plane->getGlobalBounds().left };
+	sf::Vector2f topRightCorner = { plane->getGlobalBounds().top, plane->getGlobalBounds().left + plane->getGlobalBounds().width };
+	sf::Vector2f bottomLeftCorner = { plane->getGlobalBounds().top + plane->getGlobalBounds().height, plane->getGlobalBounds().left };
+	sf::Vector2f bottomRightCorner = { plane->getGlobalBounds().top + plane->getGlobalBounds().height, plane->getGlobalBounds().left + plane->getGlobalBounds().width };
+
+	if (pointIntersection(*explosion, topLeftCorner) || pointIntersection(*explosion, topRightCorner) || pointIntersection(*explosion, bottomLeftCorner) || pointIntersection(*explosion, bottomRightCorner))
+		collision = true;
+
+	return collision;
+}
+
+
+bool Plane::pointIntersection(sf::CircleShape circle, sf::Vector2f point)
+{
+	float radius = circle.getLocalBounds().height / 2;
+	sf::Vector2f distance;
+	sf::Vector2f center;
+	bool intersects = false;
+
+	//Change in x from center of sprite to outside
+	center = sf::Vector2f((sf::FloatRect(circle.getGlobalBounds()).left) + (sf::FloatRect(circle.getGlobalBounds()).width / 2), (sf::FloatRect(circle.getGlobalBounds()).top) + sf::FloatRect(circle.getGlobalBounds()).height / 2);
+	distance.x = point.x - center.x;
+	distance.y = point.y - center.y;
+	if (distance.x * distance.x + distance.y * distance.y <= radius * radius)
+		intersects = true;
+
+	return intersects;
 }
--- coolGameEngine/coolGameEngine/Plane.h
+++ coolGameEngine/coolGameEngine/Plane.h
@@ -24,6 +24,11 @@ public:
 	void setMissilesLeft(int);
 	int getMissilesLeft();
 	int getTotalMissiles();
+	int launchMissiles(sf::RenderWindow *, SystemManager *); //Returns 0 if it fails to launch, 1 if it successfully launches
+	void update(sf::RenderWindow *, SystemManager *);
+	void launchPlane(sf::RenderWindow *, SystemManager*, int);
+	bool intersection(sf::CircleShape *, sf::Sprite *);
+	bool pointIntersection(sf::CircleShape, sf::Vector2f);
 	int launchMissiles(sf::RenderWindow *); //Returns 0 if it fails to launch, 1 if it successfully launches
 	void update(sf::RenderWindow *);
 	void launchPlane(sf::RenderWindow *, int);
